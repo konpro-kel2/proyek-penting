69619807766a7f1f52acf04df85cfe380be7f28d
IqbalHadiSubekti <iqbalhadist98@gmail.com>
2017-12-12 21:31:34 +0700

    desa

---
+++ tebak_angka.py
@@ -0,0 +1,39 @@
+import random
+
+def randnumber():
+	print("Tebak angka antara 1 sd 100")
+	number = random.randint(1,100)
+	return number
+
+def userinput():
+	putar = True
+	while putar:
+		guest = input("Masukkan angka tebakan: ")
+		try:
+	   		number = int(guest)
+		except ValueError:
+		    print("Input harus berupa angka")
+		else:
+		    return int(guest)
+		    putar = False
+
+def main():
+	tebak = True
+	number = randnumber()
+	angkauser = userinput()
+	num_tebak = 0
+	while tebak:
+		num_tebak += 1
+		if (angkauser==number):
+			print("Tebakan benar, angkanya adalah", number)
+			print("Tertebak dalam %d tebakan" % num_tebak)
+			tebak = False
+		elif (angkauser<number):
+			print("Angka terlalu kecil")
+			angkauser = userinput()
+		else:
+			print("Angka terlalu besar")
+			angkauser = userinput()
+	print("Terimakasih sudah bermain")
+
+main()
